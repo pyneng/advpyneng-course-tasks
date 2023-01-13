4c216fbe7f542790e44c010f2ef40894a4b297c2
pyneng <pyneng.course@gmail.com>
2023-01-13 17:41:28 +0000

    UPD 1.5 telnetlib -> paramiko

--- exercises/01_pytest_basics/task_1_5.py
+++ exercises/01_pytest_basics/task_1_5.py
@@ -2,9 +2,9 @@
 """
 Задание 1.5
 
-Написать тесты для класса CiscoTelnet. Тесты должны проверять:
+Написать тесты для класса CiscoSSH. Тесты должны проверять:
 
-* создание подключения Telnet при создании экземпляра. Один из признаков
+* создание подключения SSH при создании экземпляра. Один из признаков
   тут - отсутствие исключений при создании экземлпяра. Также можно
   проверить значение self.prompt
 * проверка параметра secret в методе __init__ - при значении по умолчанию None
@@ -29,86 +29,110 @@
 
 Тест(ы) написать в файле заданий.
 
-Ограничение: класс менять нельзя.
+Ограничение: класс менять нельзя (можно поправить по мелочи, если есть какая-то ошибка).
 Для заданий этого раздела нет тестов для проверки тестов.
 """
-import time
-import telnetlib
+from pprint import pprint
 import re
+import time
+import paramiko
 
-import yaml
 
-
-class CiscoTelnet:
+class CiscoSSH:
     def __init__(
         self,
         host,
         username,
         password,
-        secret=None,
-        disable_paging=True,
-        read_timeout=5,
-        encoding="utf-8",
+        secret,
+        pause=0.2,
+        max_read=100000,
+        read_timeout=2,
     ):
         self.host = host
-        self.username = username
-        self.prompt = ">"
+        self.pause = pause
+        self.max_read = max_read
         self.read_timeout = read_timeout
-        self.encoding = encoding
 
-        self._telnet = telnetlib.Telnet(host)
-        self._read_until("Username")
-        self._write_line(username)
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.connect(
+            hostname=host,
+            username=username,
+            password=password,
+            look_for_keys=False,
+            allow_agent=False,
+        )
+        self._ssh = client.invoke_shell()
+        self._send_command("enable")
         self._read_until("Password")
-        self._write_line(password)
+        self._send_command(secret)
+        self._read_until("#")
+        self._send_command("terminal length 0")
+        self._read_until("#")
+        self.prompt = self.get_prompt()
 
-        match_index, match_obj, output = self._telnet.expect(
-            [b">", b"#"], timeout=self.read_timeout
-        )
-        if not match_obj:
-            raise ValueError("Cisco prompt not found")
-        self.hostname = re.search(r"(\S+)[#>]", output.decode(self.encoding)).group(1)
-        if match_index == 0 and secret:
-            self._write_line("enable")
-            self._read_until("Password")
-            self._write_line(secret)
-            self._read_until("#")
-            self.prompt = "#"
-        elif match_index == 1:
-            self.prompt = "#"
-        if disable_paging:
-            self._write_line("terminal length 0")
-            self._read_until(self.prompt)
+    def get_prompt(self):
+        self._send_command("sh clock")
+        time.sleep(self.pause)
+        output = self._read_until("[#>]")
+        match = re.search(r"\S+[>#]", output)
+        if match:
+            return match.group()
+        else:
+            return "#"
 
-    def _read_until(self, line):
-        output = self._telnet.read_until(
-            line.encode(self.encoding), timeout=self.read_timeout
-        )
-        return output.decode(self.encoding).replace("\r\n", "\n")
+    def _send_command(self, command):
+        self._ssh.send(f"{command}\n")
 
-    def _write_line(self, line):
-        self._telnet.write(f"{line}\n".encode(self.encoding))
+    def _read_until_prompt(self):
+        output = self._read_until(self.prompt)
+        return output
 
-    def send_show_command(self, command):
-        self._write_line(command)
-        command_output = self._read_until(self.prompt)
-        return command_output
+    def _read_until(self, regex):
+        self._ssh.settimeout(self.read_timeout)
+        output = ""
+        while True:
+            time.sleep(0.2)
+            try:
+                part = self._ssh.recv(self.max_read).decode("utf-8")
+                output += part
+            except OSError:
+                break
+            match = re.search(regex, output)
+            if match:
+                break
+        return output
+
+    def send_show_command(self, show_command):
+        self._send_command(show_command)
+        output = self._read_until_prompt()
+        return output
+
+    def send_config_commands(self, commands):
+        if type(commands) == str:
+            commands = ["conf t", commands, "end"]
+        else:
+            commands = ["conf t", *commands, "end"]
+        output = ""
+        for cmd in commands:
+            self._send_command(cmd)
+            time.sleep(self.pause)
+        output += self._read_until_prompt()
+        return output
+
+    def close(self):
+        self._ssh.close()
 
     def __enter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self._telnet.close()
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
 
 
 if __name__ == "__main__":
-    r1_params = {
-        "host": "192.168.100.1",
-        "username": "cisco",
-        "password": "cisco",
-        "secret": "cisco",
-    }
-    with CiscoTelnet(**r1_params) as r1:
+    with CiscoSSH("192.168.100.1", "cisco", "cisco", "cisco") as r1:
         print(r1.send_show_command("sh clock"))
         print(r1.send_show_command("sh ip int br"))
         print(r1.send_show_command("sh run | i hostname"))
